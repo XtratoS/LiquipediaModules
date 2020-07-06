9eafa872ca2950a5411a1af9608a4c78ef3a195f
XtratoS <ultimate.nuker@yahoo.com>
2020-07-06 05:31:30 +0200

    Created ArgumentFetcher which fetches a subtemplate into arguments for the main template

---
+++ ArgumentFetcher.lua
@@ -0,0 +1,24 @@
+---- This module fetches the arguments from a sub-template (A template used inside another template)
+
+local p = {} --p stands for package
+
+local getArgs = require('Module:Arguments').getArgs
+local tprint = require('Module:Sandbox/TablePrinter').tprint
+
+--- Entry point
+-- Module entry point
+-- @param frame frame
+-- @return string - the fetched arguments separated by '$' symbol
+function p.main(frame)
+    args = getArgs(frame)
+    fetchedArgs = ''
+    teamName = args[1]
+    for key, val in pairs(args) do
+        if type(key) == 'string' then
+            fetchedArgs = fetchedArgs .. '$' .. teamName .. key .. '=' .. val
+        end
+    end
+    return teamName .. fetchedArgs
+end
+
+return p
