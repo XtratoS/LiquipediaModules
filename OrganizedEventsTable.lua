578bafd1b6d2d4d67a644b81df102fff26c816a6
XtratoS <ultimate.nuker@yahoo.com>
2020-12-31 01:19:46 +0200

    created a new lua module

---
+++ OrganizedEventsTable.lua
@@ -0,0 +1,12 @@
+local p = {} --p stands for package
+
+local getArgs = require('Module:Arguments').getArgs
+local inspect = require('Module:Sandbox/inspect').inspect
+
+--- Entry point.
+-- This is the entry point of the Module.
+-- @tparam frame frame
+-- @return 
+function p.main(frame)
+    local args = getArgs(frame)
+end
